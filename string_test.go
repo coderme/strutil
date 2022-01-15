032072f4b7d6a95bcfd78db671545976f8a0ecda
CoderMe <srcii@coderme.com>
2022-01-15 18:23:37 +0100

    Updating: string_test.go
    Signed-off-by: CoderMe <srcii@coderme.com>

---
+++ string_test.go
@@ -0,0 +1,41 @@
+package strutil
+
+import (
+	"testing"
+)
+
+// TestSubstring tests Substring for returing correct values on various input.
+func TestSubstring(t *testing.T) {
+	t.Parallel()
+
+	type Case struct {
+		Value, Result       string
+		Start, End          int
+		TrimSpace, Ellipsis bool
+	}
+
+	cases := []Case{
+		{
+			Value:     "abcdef",
+			Start:     0,
+			End:       2,
+			TrimSpace: false,
+			Ellipsis:  false,
+			Result:    "ab",
+		},
+		{
+			Value:     "abcdef",
+			Start:     4,
+			End:       9,
+			TrimSpace: false,
+			Ellipsis:  false,
+			Result:    "ef",
+		},
+		{
+			Value:     "abcdef",
+			Start:     1,
+			End:       3,
+			TrimSpace: false,
+			Ellipsis:  false,
+			Result:    "bc",
+		},
