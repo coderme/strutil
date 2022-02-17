cc0ddec7818869a1026d9363b0aa4708caf7bc37
CoderMe <srcii@coderme.com>
2022-02-17 04:47:19 +0100

    Updating: string.go
    Signed-off-by: CoderMe <srcii@coderme.com>

--- string.go
+++ string.go
@@ -94,3 +94,4 @@ func TrimPrefixPhrases(s string, trimSpace bool, phrases ...string) string {
 // IsNumeric returns true if s contains only utf8 numbers.
 func IsNumeric(s string, trimSpace bool) bool {
 	if trimSpace {
+		s = strings.TrimSpace(s)
