238d3aa67b993d61da60f8bc068d1e4ef8ecc32b
CoderMe <srcii@coderme.com>
2022-03-03 23:07:26 +0100

    Updating: string_test.go
    Signed-off-by: CoderMe <srcii@coderme.com>

--- string_test.go
+++ string_test.go
@@ -121,3 +121,4 @@ func TestSubstring(t *testing.T) {
 	for _, v := range cases {
 		if r := Substring(v.Value, v.Start, v.End, v.TrimSpace, v.Ellipsis); r != v.Result {
 			t.Error("expected:", v.Result,
+				", but found:", r,
