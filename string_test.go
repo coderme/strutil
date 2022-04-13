b8f6c301c2f8e1131bb680ce79677c0904d357d2
CoderMe <srcii@coderme.com>
2022-04-13 08:18:41 +0200

    Updating: string_test.go
    Signed-off-by: CoderMe <srcii@coderme.com>

--- string_test.go
+++ string_test.go
@@ -178,3 +178,4 @@ func TestTrimPhrases(t *testing.T) {
 
 // TestTrimSuffixPhrases tests TrimSuffixPhrases for trimming phrases from the end of a string.
 func TestTrimSuffixPhrases(t *testing.T) {
+	t.Parallel()
