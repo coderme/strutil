7ad02ab4f6152c43c2b32efd818a09b0e4013b81
CoderMe <srcii@coderme.com>
2022-05-01 09:55:09 +0200

    Updating: string.go
    Signed-off-by: CoderMe <srcii@coderme.com>

--- string.go
+++ string.go
@@ -229,3 +229,4 @@ func IsIEqual(trim bool, args ...string) bool {
 
 // IContainsAny checks if any args is in the first arg.
 func IContainsAny(s string, args ...string) bool {
+	s = strings.ToLower(s)
