162e12ee4f4317a942804c0613283fce1c048621
CoderMe <srcii@coderme.com>
2022-05-04 10:42:42 +0200

    Updating: string.go
    Signed-off-by: CoderMe <srcii@coderme.com>

--- string.go
+++ string.go
@@ -243,3 +243,4 @@ func IContainsAny(s string, args ...string) bool {
 func IcontainsAnyPhrase(s string, args ...string) bool {
 	s = strings.ToLower(s)
 	for _, a := range args {
+		if strings.Contains(s, strings.ToLower(a)) {
