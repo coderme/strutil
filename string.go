5c21e5012e78e2aea4ca8c072e9bddc528582c8b
CoderMe <srcii@coderme.com>
2022-05-12 10:34:43 +0200

    Updating: string.go
    Signed-off-by: CoderMe <srcii@coderme.com>

--- string.go
+++ string.go
@@ -263,3 +263,4 @@ func ContainsAnyPhrase(s string, args ...string) bool {
 }
 
 // ContainsAll checks if all args are in the first arg, case sensitive.
+func ContainsAll(s string, args ...string) bool {
