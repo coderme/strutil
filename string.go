13c7c245a688463509d60f1ea70879ccc87e99a4
CoderMe <srcii@coderme.com>
2022-05-18 09:54:24 +0200

    Updating: string.go
    Signed-off-by: CoderMe <srcii@coderme.com>

--- string.go
+++ string.go
@@ -274,3 +274,4 @@ func ContainsAll(s string, args ...string) bool {
 }
 
 // ContainsIAll checks if all args are in the first arg, case isensitive.
+func ContainsIAll(s string, args ...string) bool {
